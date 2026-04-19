b25b7c31592aa53aa633fa91e66f2d9efd8598e3
Damali Boyd <damaliboyd@gmail.com>
2026-04-19 13:39:42 -0400

    car rides uses proper dialogue now

--- scenes/car_ride/car_ride.gd
+++ scenes/car_ride/car_ride.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-## Reusable car ride transition. Reads GameManager.current_job for dialogue and visual intensity.
+## Reusable car ride transition. Reads GameManager.pending_job_index for dialogue
+## and visual intensity, and transitions to GameManager.pending_job_scene when done.
 
 @onready var camera: Camera3D = $Camera3D
 @onready var scrolling_env: Node3D = $ScrollingEnvironment
@@ -21,7 +22,7 @@ var job_configs := [
 
 func _ready() -> void:
 	dialogue_box.add_to_group("dialogue_box")
-	var config: Dictionary = job_configs[mini(GameManager.current_job, job_configs.size() - 1)]
+	var config: Dictionary = job_configs[mini(GameManager.pending_job_index, job_configs.size() - 1)]
 	scroll_speed = config["scroll_speed"]
 	$WorldEnvironment.environment.fog_density = config["fog_density"]
 
@@ -47,7 +48,7 @@ func _process(delta: float) -> void:
 	scrolling_env.position.z += scroll_speed * delta
 
 	# Flicker streetlights
-	var config: Dictionary = job_configs[mini(GameManager.current_job, job_configs.size() - 1)]
+	var config: Dictionary = job_configs[mini(GameManager.pending_job_index, job_configs.size() - 1)]
 	for light: Node in get_tree().get_nodes_in_group("streetlight"):
 		if light is OmniLight3D and randf() < config["flicker"]:
 			light.light_energy = randf_range(0.1, 0.8)
@@ -58,7 +59,7 @@ func _process(delta: float) -> void:
 
 
 func _start_dialogue() -> void:
-	var key := "car_ride_%d" % GameManager.current_job
+	var key := "car_ride_%d" % GameManager.pending_job_index
 	GameManager.start_dialogue(key)
 
 	dialogue_box.dialogue_finished.connect(_on_dialogue_finished, CONNECT_ONE_SHOT)
@@ -75,5 +76,7 @@ func _transition_to_job() -> void:
 	GameManager.set_state(GameManager.GameState.JOB_ARRIVING)
 	TransitionOverlay.fade_out(2.0)
 	await TransitionOverlay.fade_out_finished
-	var path: String = GameManager.job_scene_paths[GameManager.current_job]
-	GameManager.transition_to_scene(path)
+	if GameManager.pending_job_scene:
+		GameManager.transition_to_packed_scene(GameManager.pending_job_scene)
+	else:
+		push_warning("car_ride: no pending_job_scene set; cannot transition")
